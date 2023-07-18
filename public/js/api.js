e939c86547265511cd46d5fb550446c6add3d202
Caleb <agyakojo4321@gmail.com>
2023-07-18 15:31:10 +0000

    ui for weather app done

---
+++ public/js/api.js
@@ -0,0 +1,10 @@
+// getting current location weather
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=40f203f9880d84645820b0bf47828823
+
+/**
+ * https://weatherapi-com.p.rapidapi.com/current.json?q={Accra}
+ */
+
+const me = new Date("2023-07-18 13:25");
+
+console.log(me.toLocaleTimeString());
