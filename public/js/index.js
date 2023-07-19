06ff00c2464d9a9c3c7c0a30886f24804780a40d
Caleb <agyakojo4321@gmail.com>
2023-07-19 09:15:38 +0000

    working on api key

--- public/js/index.js
+++ public/js/index.js
@@ -9,8 +9,8 @@ const fetchDataByCoordinate = function ({ longitude, latitude }) {
 		`https://weatherapi-com.p.rapidapi.com/current.json?q=${latitude},${longitude}`,
 		{
 			headers: {
-				'X-RapidAPI-Key': 'f7ce8a6e8cmsh987eaefb4062f39p1c7499jsn78a8205605a6',
-				'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+				'X-RapidAPI-Key': key,
+				'X-RapidAPI-Host': host,
 			},
 		}
 	)
@@ -24,8 +24,8 @@ const fetchDataByCity = async function (city) {
 	return async function (url) {
 		const response = await fetch(`${url}?q=${city}`, {
 			headers: {
-				'X-RapidAPI-Key': 'f7ce8a6e8cmsh987eaefb4062f39p1c7499jsn78a8205605a6',
-				'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+				'X-RapidAPI-Key': key,
+				'X-RapidAPI-Host': host,
 			},
 		});
 		return response;
